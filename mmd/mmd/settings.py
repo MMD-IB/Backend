e8d33975a0bb7c558e432bbca814d534c053de48
unknown <gspptesse@gmail.com>
2026-01-15 17:47:45 +0100

    Modifica in settings.py con aggiunta di db SQL ATTO III

--- mmd/mmd/settings.py
+++ mmd/mmd/settings.py
@@ -81,7 +81,7 @@ WSGI_APPLICATION = 'mmd.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
+    'sqlite': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     },
