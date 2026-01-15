53179bee8517834273c1597dcb685b55e54b811e
unknown <gspptesse@gmail.com>
2026-01-15 17:45:39 +0100

    Modifica in settings.py con aggiunta di db SQL ATTO II

--- mmd/mmd/settings.py
+++ mmd/mmd/settings.py
@@ -84,11 +84,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-DATABASES = {
-    "default": {
+    },
+    "postgres": {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": os.getenv("DB_NAME"),
         "USER": os.getenv("DB_USER"),
