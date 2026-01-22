6823f58a2303e452550243e63bba903472993110
Gking2004 <gspptesse@gmail.com>
2026-01-22 09:59:46 +0100

    Aggiunta di visualizzazione dati user,update e logout

--- mmd/user/views.py
+++ mmd/user/views.py
@@ -4,13 +4,61 @@ from user.models import MyUser
 
 
 def user_index(request):
-    context = {"appname":"User"}
-    
+    if "id_user" not in request.session or request.session["id_user"] is None:
+        return render(request,"home.html")
+    context = {
+        "appname":"User",
+        "id_user": request.session["id_user"],
+        "user": get_user(request)
+    }
+    if request.method == "POST":
+        azione = request.POST.get("azione")             
+        if azione == "logout":
+            return logout(request)
+    if request.method == "PUT":
+        extra = update_user(request)
+        context.update(extra)
     return render(request,"user-index.html",context)
 
-def get_users(request):
-    context = {"appname":"Users"}
 
-    users = MyUser.objects.all()
-    context["users"] = users
-    return render(request,context)
+def get_user(request):
+    user_id = request.session["id_user"]
+    return MyUser.objects.filter(id=user_id).first()
+
+
+def logout(request):
+    request.session["id_user"] = None
+    return render(request,"home.html", {"appname":"Logout"})
+
+
+def update_user(request):
+    user_id = request.session["id_user"]
+    user = get_object_or_404(MyUser, id=user_id)
+
+    name = request.POST.get("name")
+    surname = request.POST.get("surname")
+    email = request.POST.get("email")
+
+    if not any([name, surname, email]):
+        return {
+            "error": "All fields are empty!",
+            "user": user
+        }
+
+    if name:
+        user.name = name
+    if surname:
+        user.surname = surname
+    if email:
+        user.email = email
+    user.save()
+
+    return {
+        "success": "User updated correctly",
+        "user": user
+    }
+    
+
+
+
+
