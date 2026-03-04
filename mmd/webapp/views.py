8fc1bc2ced5fc4670fb4fc4400d7168d3832387d
Giammarco De Lauretis <giammarco1907@gmail.com>
2026-03-04 16:37:40 +0100

    Web app views

--- mmd/webapp/views.py
+++ mmd/webapp/views.py
@@ -8,48 +8,59 @@ def index(request):
     context = {
         'app_name': 'Utente',
     }
+    if not context:
+        return None
     return render(request, 'home.html', context)
 
 
 def login(request):
-    if request.method == "POST":
-        form = LoginForm(request.POST)
-        if form.is_valid():
-            user = login_user(
-                form.cleaned_data["email"],
-                form.cleaned_data["password"]
-            )
-            if user:
-                request.session["id_user"] = user["id"]
-                return redirect("dashboard")
-            else:
-                return render(request, "login.html", {"form": form, "error": "Credenziali errate"})
-    else:
-        form = LoginForm()
+    try:
+        if request.method == "POST":
+            form = LoginForm(request.POST)
+            if form.is_valid():
+                user = login_user(
+                    form.cleaned_data["email"],
+                    form.cleaned_data["password"]
+                )
+                if user:
+                    request.session["id_user"] = user["id"]
+                    return redirect("dashboard")
+                else:
+                    return render(request, "login.html", {"form": form, "error": "Credenziali errate"})
+        else:
+            form = LoginForm()
 
-    return render(request, "login.html", {"form": form})
+        return render(request, "login.html", {"form": form})
+    except Exception as e :
+        print("error: ",e)
+        return e
+
 
 def register(request):
-    if request.method == "POST":
-        form = RegisterForm(request.POST)
-        if form.is_valid():
-            name = form.cleaned_data['name']
-            surname = form.cleaned_data['surname']
-            email = form.cleaned_data['email']
-            password = form.cleaned_data['password']
-            try:
-                if register_user(name, surname, email, password):
-                    return redirect('login')
-                else:
+    try:
+        if request.method == "POST":
+            form = RegisterForm(request.POST)
+            if form.is_valid():
+                name = form.cleaned_data['name']
+                surname = form.cleaned_data['surname']
+                email = form.cleaned_data['email']
+                password = form.cleaned_data['password']
+                try:
+                    if register_user(name, surname, email, password):
+                        return redirect('login')
+                    else:
+                        return render(request, "register.html", {"form": form, "error": "Errore durante la registrazione"})
+                except Exception as e:
+                    print(e)
                     return render(request, "register.html", {"form": form, "error": "Errore durante la registrazione"})
-            except Exception as e:
-                print(e)
-                return render(request, "register.html", {"form": form, "error": "Errore durante la registrazione"})
-    else:
-        form = RegisterForm()
-    context = {
-        'app_name': 'Utente',
-        'form': form,
-    }
-    return render(request, 'register.html', context)
+        else:
+            form = RegisterForm()
+        context = {
+            'app_name': 'Utente',
+            'form': form,
+        }
+        return render(request, 'register.html', context)
+    except Exception as e:
+        print("Error: ",e)
+        return e
 
